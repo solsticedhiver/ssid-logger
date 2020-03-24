c22df6f0fbc713bf17115a7a29e71bbe63bce26e
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2020-03-24 14:02:49 +0100

    Improve checks in python script

--- sqlite3_to_csv.py
+++ sqlite3_to_csv.py
@@ -4,6 +4,8 @@ import sqlite3
 import csv
 import argparse
 from datetime import datetime
+import os.path
+import sys
 
 VERSION = '0.1'
 CSV_HEADER_1 = ['WigleWifi-1.4', f'appRelease={VERSION}', 'model=ssid-logger',
@@ -15,19 +17,30 @@ CSV_HEADER_2 = ['MAC', 'SSID', 'AuthMode', 'FirstSeen', 'Channel', 'RSSI',
 def main():
     parser = argparse.ArgumentParser(description='Convert sqlite3 beacon.db to a csv file')
     parser.add_argument('-i', '--input', required=True, help='input sqlite3 db file')
+    parser.add_argument('--force', action='store_true', default=False, help='force overwrite of existing file')
     parser.add_argument('-o', '--output', required=True, help='output csv file name')
     args = parser.parse_args()
-    print(args.input)
 
-    conn = sqlite3.connect(f'file:{args.input}?mode=ro', uri=True)
-    c = conn.cursor()
-    sql = 'pragma query_only = on;'
-    c.execute(sql)
-    sql = 'pragma temp_store = 2;' # to store temp table and indices in memory
-    c.execute(sql)
-    sql = 'pragma journal_mode = off;' # disable journal for rollback (we don't use this)
-    c.execute(sql)
-    conn.commit()
+    if not os.path.exists(args.input):
+        print(f'Error: {args.input} not found', file=sys.stderr)
+        sys.exit(-1)
+    if os.path.exists(args.output) and not args.force:
+        print(f'Error: {args.output} already exists. Use --force to overwrite', file=sys.stderr)
+        sys.exit(-1)
+
+    try:
+        conn = sqlite3.connect(f'file:{args.input}?mode=ro', uri=True)
+        c = conn.cursor()
+        sql = 'pragma query_only = on;'
+        c.execute(sql)
+        sql = 'pragma temp_store = 2;' # to store temp table and indices in memory
+        c.execute(sql)
+        sql = 'pragma journal_mode = off;' # disable journal for rollback (we don't use this)
+        c.execute(sql)
+        conn.commit()
+    except sqlite3.DatabaseError as d:
+        print(f'Error: {args.input} is not an sqlite3 db', file=sys.stderr)
+        sys.exit(-1)
 
     sql = '''select ap.bssid, ap.ssid, authmode.mode, ts, channel, rssi, lat, lon, alt, acc  from beacon
         inner join ap on ap.id=beacon.ap
